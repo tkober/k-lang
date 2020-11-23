8ea67d75d7435ed17e7d9813ff7aa7e18c47d8e5
Thorsten Kober <thorsten.kober@softgarden.de>
2020-11-23 12:19:06 +0100

    Chapter 2: AST

--- llvm_tutorial/main.cpp
+++ llvm_tutorial/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 enum Token {
     token_eof = -1,
@@ -77,6 +78,68 @@ static int getToken() {
         return thisCharacter;
     }
 }
+
+/// ExpressionAst - base class for all expression nodes.
+class ExpressionAst {
+public:
+    virtual ~ExpressionAst() {}
+};
+
+/// NumberExpressionAst - Expression class for all numeric literals like "1.0".
+class NumberExpressionAst : public ExpressionAst {
+    double value;
+
+public:
+    NumberExpressionAst(double value) : value(value) {}
+};
+
+/// VariableExpressionAst - Expression class for referencing a variable like "foo".
+class VariableExpressionAst : public ExpressionAst {
+    std::string name;
+
+public:
+    VariableExpressionAst(const std::string &name) : name(name) {}
+};
+
+/// BinaryExpressionAst - Expression class for a binary operator.
+class BinaryExpressionAst : public ExpressionAst {
+    char op;
+    std::unique_ptr<ExpressionAst> lhs, rhs;
+
+public:
+    BinaryExpressionAst(char op, std::unique_ptr<ExpressionAst> lhs, std::unique_ptr<ExpressionAst> rhs) : op(op), lhs(std::move(lhs)), rhs(std::move(rhs)) {}
+};
+
+// CallExpressionAst - Expression class for function calls.
+class CallExpressionAst : public ExpressionAst {
+    std::string callee;
+    std::vector<std::unique_ptr<ExpressionAst>> arguments;
+
+public:
+    CallExpressionAst(const std::string &callee, std::vector<std::unique_ptr<ExpressionAst>> arguments) : callee(callee), arguments(std::move(arguments)) {}
+};
+
+/// PrototypeAst - This class represents the "prototype" of a function,
+/// which captures its name and its argument names (thus implicitly the number of arguments) it takes.
+class PrototypeAst {
+    std::string name;
+    std::vector<std::string> arguments;
+
+public:
+    PrototypeAst(const std::string &name, std::vector<std::string> arguments) : name(name), arguments(std::move(arguments)) {}
+
+    const std::string &getName() const { return name; }
+};
+
+/// FunctionAst - This class represents a function definition itself.
+class FunctionAst {
+    std::unique_ptr<PrototypeAst> prototype;
+    std::unique_ptr<ExpressionAst> body;
+
+public:
+    FunctionAst(std::unique_ptr<PrototypeAst> prototype, std::unique_ptr<ExpressionAst> body) : prototype(std::move(prototype)), body(std::move(body)) {}
+};
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
